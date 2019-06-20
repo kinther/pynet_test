8cc76448634c683e7780b1e313c61ce355a64f8e
Patrick Walton <xdelaranx@gmail.com>
2019-06-20 13:43:19 -0700

    added string exercise 1

---
+++ str_ex1.py
@@ -0,0 +1,11 @@
+var_name1 = "docu"
+var_name2 = "sign"
+var_name3 = "rules"
+
+var_name4 = input("Please enter a fourth name: ")
+
+print("{:>30}".format(var_name1))
+print("{:>30}".format(var_name2))
+print("{:>30}".format(var_name3))
+print("{:>30}".format(var_name4))
+
