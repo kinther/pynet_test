788609faf91dc152711442d1b757202c748324d1
Patrick Walton <xdelaranx@gmail.com>
2019-07-09 09:14:47 -0700

    updated and verified regex lesson 2

--- regex_ex2.py
+++ regex_ex2.py
@@ -4,19 +4,38 @@
 import re
 from pprint import pprint
 
-# Create dictionary to hold data
+# Create data structures
 dict = {
-    "address": "",
-    "vlan": "",
-    "type": "",
-    "ports": "",
 }
+
+mac_dict = {
+    "ports": "",
+    "vlan": "",
+    "type":"",
+}
+
+ports = []
 
 # Open file and assign to variable
 with open("show_mac_multicast.txt") as f:
     output = f.read()
 
 # Define regex patterns to search for
-patterns = "^\s+(\d+)\s+([0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(.*)$"
+pattern = r"^\s+(\d+)\s+([0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(.*)$"
 
-# Parse data for match groups and assign to dictionary values
+# Parse data for match groups
+for line in output.splitlines():
+    match = re.search(pattern, line)
+
+# Split ports along delineator and paste into list format
+ports = match.group(4).split(",")
+
+# Assign matches to dictionary values
+mac_dict["vlan"] = match.group(1)
+mac_dict["type"] = match.group(3)
+mac_dict["ports"] = match.group(4)
+
+dict[match.group(2)] = mac_dict
+
+# Print data out pretty like
+pprint(dict)
