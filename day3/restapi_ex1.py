d19207cb8a4b404ec58ce1ca75bb5a09cda1fddc
Patrick Walton <xdelaranx@gmail.com>
2019-07-16 13:39:46 -0700

    added basic rest_api script to perform HTTP GET

--- day3/restapi_ex1.py
+++ day3/restapi_ex1.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python
 
 # Usage
-# Pulls GET information from a URL
+# Opens a REST API and performs a GET
 
 # Imports
 import requests
+from urllib3.exceptions import InsecureRequestWarning
 from pprint import pprint
 
-from urllib3.exceptions import InsecureRequestWarning
-
-# Ignore insecure warings for sites with self-signed certificates
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
-
-url = "https://netbox.lasthop.io/api/"
+# Variables
 http_headers = {"accept": "application/json; version=2.4;"}
-response = requests.get(url, headers=http_headers, verify=False)
+url = "https://netbox.lasthop.io/api"
 
-# Print out information on the URL we got
+# Disable self-signed certificate complaints
+requests.urllib3.disable_warnings(InsecureRequestWarning)
 
-print(dir(response))
+# Try to open remote site
+response = requests.get(url, http_headers, verify=False)
 
+# Print out data types from response
+pprint(dir(response))
+
+# Print out whether the response was OK or not
+print("Was the response from the server ok? - {}".format(response.ok))
+
+# Print out the status code from the HTTP GET
+print("Status code from the HTTP GET was {}.".format(response.status_code))
+
+# Print out the URL we queried with HTTP GET
+print("The url we queried was {}.".format(response.url))
+
+# Print out any text that was called
+print("Text from the HTTP GET was {}.".format(response.text))
+
+# Print out the json data format pretty like
+print("Json format:")
 pprint(response.json())
-pprint(response.text)
-pprint(response.status_code)
-pprint(response.ok)
